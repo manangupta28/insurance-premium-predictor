2cf2dccd56b1d757d8ea0736c404f37053bd12ad
manangupta28 <hatakekakashi1528@gmail.com>
2025-07-28 09:53:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
 import torch.nn as nn
 
 class InsuranceModel(nn.Module):
-    def __init__(self, input_size):
-        super(InsuranceModel, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(input_size, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 1)
-        )
+  def __init__(self,input_dim):
+    super(InsuranceModel , self).__init__()
 
-    def forward(self, x):
-        return self.fc(x)
+    self.fc1 = nn.Linear(input_dim , 64)
+    self.fc2 = nn.Linear(64,32)
+    self.output = nn.Linear(32,1)
+    self.relu = nn.ReLU()
+
+  def forward(self,x):
+    x = self.relu(self.fc1(x))
+    x = self.relu(self.fc2(x))
+    x = self.output(x)
+
+    return x
